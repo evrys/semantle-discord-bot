c46b8930a2a00d9beb02989b28563a0b0a521de0
Toggly <toggledy@gmail.com>
2022-03-29 19:22:09 +1100

    Don't need a cron trigger

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,13 @@
-import { createSlashCommandHandler } from "@glenstack/cf-workers-discord-bot";
-import { commands } from "./commands";
-import { workersReleaseNotes } from "./scheduled/rss";
+import { createSlashCommandHandler } from "@glenstack/cf-workers-discord-bot"
+import { commands } from "./commands"
 
 const slashCommandHandler = createSlashCommandHandler({
   applicationID: APPLICATION_ID,
   applicationSecret: APPLICATION_SECRET,
   publicKey: PUBLIC_KEY,
   commands,
-});
-
-// addEventListener("scheduled", (event) => {
-//   event.waitUntil(workersReleaseNotes());
-// });
+})
 
 addEventListener("fetch", (event) => {
-  event.respondWith(slashCommandHandler(event.request));
-});
+  event.respondWith(slashCommandHandler(event.request))
+})
